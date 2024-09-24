9c3b798d5d05b104af5a386a46eb58c01f5e6709
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2024-09-24 23:53:39 +0000

    migrated to anthropic.claude-3-sonnet

--- smart_mail/src/common/client_factory.py
+++ smart_mail/src/common/client_factory.py
@@ -12,36 +12,36 @@ from services.generation.ollama_generation_service import OllamaGenerationServic
 
 class ClientFactory:
     def __init__(self, settings: Settings) -> None:
-        self.settings = settings
+        self._settings = settings
 
     def create_elasticsearch_client(self) -> Elasticsearch:
-        return Elasticsearch(self.settings.elastic_search_url)
+        return Elasticsearch(self._settings.elastic_search_url)
 
     def create_postgresql_connection(self):
         return psycopg2.connect(
-            dbname=self.settings.postgres_db,
-            user=self.settings.postgres_user,
-            password=self.settings.postgres_password,
-            host="localhost",  # Assuming localhost; adjust as needed
-            port=self.settings.postgres_port,
+            dbname=self._settings.postgres_db,
+            user=self._settings.postgres_user,
+            password=self._settings.postgres_password,
+            host=self._settings.postgres_host,
+            port=self._settings.postgres_port,
         )
 
     def create_database_manager(self) -> DatabaseManager:
-        database_manager = DatabaseManager(self.settings)
+        database_manager = DatabaseManager(self._settings)
 
         return database_manager
 
     def create_generation_service(self) -> GenerationService:
-        if self.settings.use_local_llm:
-            return OllamaGenerationService(self.settings)
+        if self._settings.use_local_llm:
+            return OllamaGenerationService(self._settings)
 
-        return AwsGenerationService(self._create_bedrock_client())
+        return AwsGenerationService(self._settings, self._create_bedrock_client())
 
     def _create_bedrock_client(self) -> BedrockRuntimeClient:
-        session: Session = boto3.Session(profile_name="private")
+        session: Session = boto3.Session(profile_name=self._settings.aws_configuration_profile_name)
+
         bedrock_runtime: BedrockRuntimeClient = session.client(
-            # because of Amazon Titan Text Premier. The previous value: eu-central-1
-            "bedrock-runtime", region_name="us-east-1"
+            "bedrock-runtime", region_name=self._settings.aws_region_name
         )
 
         return bedrock_runtime
--- smart_mail/src/common/settings.py
+++ smart_mail/src/common/settings.py
@@ -1,9 +1,32 @@
 import os
 from typing import Any
+import boto3
+from boto3.session import Session
 
 
 class Settings:
     def __init__(self):
+        """
+        Initializes the Settings object by loading environment variables and AWS configuration.
+
+        Attributes:
+            elastic_search_url (str): URL for the Elasticsearch instance.
+            index_name (str): Name of the Elasticsearch index.
+            postgres_user (str): Username for the PostgreSQL database.
+            postgres_password (str): Password for the PostgreSQL database.
+            postgres_port (str): Port number for the PostgreSQL database.
+            postgres_db (str): Name of the PostgreSQL database.
+            source_system (str): Identifier for the source system.
+            postgres_host (str): Hostname for the PostgreSQL database.
+            embedding_model_name (str): Name of the embedding model to use.
+            use_local_llm (bool): Flag indicating whether to use a local LLM (Language Learning Model).
+            local_llm_url (str, optional): URL for the local LLM API. Only set if USE_LOCAL_LLM is True.
+            local_llm_model_name (str, optional): Name of the local LLM model. Only set if USE_LOCAL_LLM is True.
+            aws_configuration_profile_name (str, optional): AWS configuration profile name. Only set if USE_LOCAL_LLM is False.
+            aws_region_name (str, optional): AWS region name. Only set if USE_LOCAL_LLM is False.
+            aws_model_name (str, optional): Name of the AWS model to use. Only set if USE_LOCAL_LLM is False.
+        """
+
         self.elastic_search_url = self.get_env_variable("ELASTIC_SEARCH_URL")
         self.index_name = self.get_env_variable("INDEX_NAME")
         self.postgres_user = self.get_env_variable("POSTGRES_USER")
@@ -14,10 +37,18 @@ class Settings:
         self.postgres_host = self.get_env_variable("POSTGRES_HOST")
         self.embedding_model_name = self.get_env_variable("EMBEDDING_MODEL_NAME")
 
-        self.use_local_llm = bool(self.get_env_variable("USE_LOCAL_LLM"))
+        use_local_llm_str = self.get_env_variable("USE_LOCAL_LLM")
+        self.use_local_llm = use_local_llm_str.lower() in ("true", "1", "yes", "y")
+
         if self.use_local_llm is True:
             self.local_llm_url = self.get_env_variable("LOCAL_LLM_URL")
             self.local_llm_model_name = self.get_env_variable("LOCAL_LLM_MODEL_NAME")
+            return
+
+        self.aws_configuration_profile_name = self.get_env_variable("AWS_CONFIGURATION_PROFILE_NAME")
+        session: Session = boto3.Session(profile_name=self.aws_configuration_profile_name)
+        self.aws_region_name = session.region_name if session.region_name is not None else self.get_env_variable("AWS_REGION_NAME")
+        self.aws_model_name = self.get_env_variable("AWS_MODEL_NAME")
 
     @staticmethod
     def get_env_variable(name: str) -> str:
@@ -40,4 +71,7 @@ class Settings:
             "use_local_llm": self.use_local_llm,
             "local_llm_url": self.local_llm_url if self.use_local_llm else "The 'use_local_llm' should be set to True to use this field.",
             "local_llm_model_name": self.local_llm_model_name if self.use_local_llm else "The 'use_local_llm' should be set to True to use this field.",
+            "aws_configuration_profile_name": self.aws_configuration_profile_name if not self.use_local_llm else "The 'use_local_llm' should be set to False to use this field.",
+            "aws_region_name": self.aws_region_name if not self.use_local_llm else "The 'use_local_llm' should be set to False to use this field.",
+            "aws_model_name": self.aws_model_name if not self.use_local_llm else "The 'use_local_llm' should be set to False to use this field.",
         }
