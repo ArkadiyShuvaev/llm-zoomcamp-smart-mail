80770c51f7fe678a608f8d9bf0bdceba05a36e7d
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2024-10-01 20:01:54 +0000

    added blocks to ingest project files

---
+++ mage/zoomcamp-smart-mail/smart-mail/transformers/replace_none_values_with_empty.py
@@ -0,0 +1,22 @@
+from pandas import DataFrame
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data: DataFrame, *args, **kwargs):
+    # There are data without a question
+    data['question'] = data['question'].fillna('')
+
+    return data
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
