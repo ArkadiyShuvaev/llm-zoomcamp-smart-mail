e9ecc9c86b7f814356bd247e5af0fa2a853c82b2
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2024-11-05 16:15:57 +0000

    moved NLTK investigations to notebook\utils

--- notebook/utils/text_preprocessor.py
+++ notebook/utils/text_preprocessor.py
@@ -24,3 +24,17 @@ class TextPreprocessor:
         text = re.sub(r'\d+', '', text)
 
         return text
+
+    def preprocess_text(self, text: str, lowercase: bool = True) -> str:
+        if lowercase:
+            text = text.lower()
+
+        # Remove special characters but keep spaces and German umlauts
+        #text = re.sub(r'[^0-9a-zäöüß.,\s]', '', text, flags=re.IGNORECASE)
+
+        # Remove extra whitespace
+        text = ' '.join(text.split())
+
+        text = ' '.join([word for word in text.split() if word not in self.stop_words])
+
+        return text
